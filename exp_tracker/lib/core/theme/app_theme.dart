51787997093ef13a6d5d24abe8928edcc7fd1e7d
LITHIRA-SASMITHA <lithirasasmitha@gmail.com>
2026-03-14 21:11:34 +0530

    initial project setup

---
+++ exp_tracker/lib/core/theme/app_theme.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import '../constants/app_colors.dart';
+
+class AppTheme {
+  AppTheme._();
+
+  static ThemeData get lightTheme {
+    return ThemeData(
+      primaryColor: AppColors.primary,
+      scaffoldBackgroundColor: AppColors.background,
+      colorScheme: const ColorScheme.light(
+        primary: AppColors.primary,
+        secondary: AppColors.secondary,
+        surface: AppColors.surface,
+        error: AppColors.error,
+      ),
+      appBarTheme: const AppBarTheme(
+        color: AppColors.primary,
+        elevation: 0,
+        centerTitle: true,
+        iconTheme: IconThemeData(color: Colors.white),
+        titleTextStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    );
+  }
+}
---
+++ exp_tracker/lib/presentation/screens/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Expense Tracker'),
+      ),
+      body: const Center(
+        child: Text(
+          'Welcome to Expense Tracker',
+          style: TextStyle(fontSize: 18),
+        ),
+      ),
+    );
+  }
+}
